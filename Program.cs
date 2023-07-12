0790136faea550e70b4a70cf87480adbb2a985c2
aryark <aryark3112@gmail.com>
2023-07-12 13:02:48 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12july_2
+{
+    internal class Program
+    {
+        enum Monthsofyear { Jan=1, Feb=2, Mar=3 };
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("First Month: \t"+ (int)Monthsofyear.Jan);
+            Console.WriteLine("Second Month:\t" + Monthsofyear.Feb);
+                Console.ReadKey();
+        }
+    }
+}
